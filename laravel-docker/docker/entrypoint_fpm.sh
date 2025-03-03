e360f3b0e364c196b9a555e94f6e67425b0ddaf1
Krish siddhapura <krish.siddhapura@grewon.com>
2025-03-03 12:43:58 +0530

    added supervisor processes

--- laravel-docker/docker/entrypoint_fpm.sh
+++ laravel-docker/docker/entrypoint_fpm.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# CLEAR LARAVEL CACHE
+php artisan cache:clear
+php artisan view:cache
+php artisan view:clear
+php artisan config:cache
+php artisan config:clear
+php artisan event:cache
+php artisan event:clear
+php artisan route:cache
+php artisan route:clear
+php artisan optimize:clear
+
+# STARTING SUPERVISOR
+supervisord -c /etc/supervisor/supervisord.conf
+exec "$@"
+
 # STARTING PHP FPM IN BACKGROUND
 php-fpm -D
 
